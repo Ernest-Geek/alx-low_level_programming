5eeb2d732b4ec6651ef67027262e82be38f66736
Ernest-Geek <ernestampene1@gmail.com>
2023-02-17 18:58:23 -0800

    ./6-print_numberz

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * main - Entry point
- *
- * Description: Prints all digit from 0 to 9
+ * main - Write a program that prints all single digit numbers of base 10
+ * starting from 0
+ * followed by a new line.
  * Results: Always 0
  */
 int main(void)
