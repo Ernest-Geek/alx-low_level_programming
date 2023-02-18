c2a3722ff5ca64a5ce128b581779b0270098e681
Ernest-Geek <ernestampene1@gmail.com>
2023-02-17 18:35:18 -0800

    file

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Prints all digit from 0 to 9
+ * Results: Always 0
+ */
+int main(void)
+{
+int num;
+for (num = 0; num < 10; num++)
+printf("%d", num);
+printf("\n");
+return (0);
+}
