88565e7210cd81eefb5901e389e38b4c9c930ddf
Ernest-Geek <ernestampene1@gmail.com>
2023-02-23 15:42:35 -0800

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,14 +2,13 @@
 /**
  * _isupper - uppercase letters
  * @c: char to check
- *
  * Return: 0 or 1
  */
 int _isupper(int c)
 {
-if (c >= 'A' && c <= 'Z')
+if (c >= '65' && c <= '90')
+{
 return (1);
-else
+}
 return (0);
 }
-
