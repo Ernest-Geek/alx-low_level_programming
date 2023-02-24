7c8d81c84b68ecbb99abfb27ff7b5d1877ba0a9c
Ernest-Geek <ernestampene1@gmail.com>
2023-02-23 16:08:03 -0800

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,9 +6,15 @@
  */
 int _isupper(int c)
 {
-if (c >= '65' && c <= '90')
+char uppercase = 'A';
+int isupper = 0;
+for (; uppercase <= 'z'; uppercase++)
 {
-return (1);
+if (c == uppercase)
+{
+isupper = 1;
+break;
 }
-return (0);
+}
+return (isupper);
 }
