c6ba4aeeb05e7d8ed467daac126f3b94bf5ae3c2
Ernest-Geek <ernestampene1@gmail.com>
2023-03-02 08:59:45 -0800

    3-strcmp.c

---
+++ 0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Write a function that takes a pointer to
+ * an int as parameter and updates the value it points to to 98.
+ * Return: int;
+ **/
+void reset_to_98(int *n);
+{
+*n = 98;
+}
