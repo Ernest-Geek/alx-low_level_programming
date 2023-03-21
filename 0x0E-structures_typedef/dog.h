1ca8f5f11c1230f9a6ae232f5cdb8bf5e9513c12
Ernest-Geek <ernestampene1@gmail.com>
2023-03-21 16:35:55 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - Entry point
+ * @name: name
+ * @age: age
+ * @owner: owner
+ * Description: Define struct dog with the three members
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif
